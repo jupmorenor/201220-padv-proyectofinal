039d37e1a2eb2a6295d413c88e62a47b870c6e2a
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-04 02:09:40 +0000

    Agrego una linea mas y corrijo identacion

--- src/db/dbtellevoatudestino.sql
+++ src/db/dbtellevoatudestino.sql
@@ -1,6 +1,10 @@
 create databases tellevoatudestino_jak;
 use tellevoatudestino_jak;
-create table rutas(codigoRuta varchar(3), nombreRuta varchar(20), inicioRuta varchar(20), finRuta varchar(20), tarifaRuta int);
-create table buses(codigoBus varchar(3), matriculaBus varchar(6), capacidadBus int, consumoCombustible float);
+create table rutas(codigoRuta varchar(3), nombreRuta varchar(20), 
+	inicioRuta varchar(20), finRuta varchar(20), tarifaRuta int);
+create table buses(codigoBus varchar(3), matriculaBus varchar(6), 
+	capacidadBus int, consumoCombustible float);
 create table sucursales(nombreSucursal varchar(20));
 create table paradas(codigoParada varchar(3), nombreParada varchar(20));
+create table empleados(nombreEmpleado varchar(35), identificacion varchar(10), 
+	telefono varchar(7), direccion varchar(35), fechaNac date());
