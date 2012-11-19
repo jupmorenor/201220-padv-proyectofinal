dcb42022c92169378fe4bd1267ad4e2ad3d401ee
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-19 02:01:39 +0000

    Clase Parada, preliminar.

---
+++ src/nucleo/Parada.java
@@ -0,0 +1,57 @@
+/* 
+ * ------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas 
+ * 
+ * Parada.java
+ * 
+ * Proyecto de primer corte de Programacion Avanzada
+ * Presentado por:
+ * Juan Pablo Moreno Rico - 20111020059
+ * Anderson Orlando Ramirez Lamprea - 20111020072
+ * Karen Vanessa Angulo Sogamoso - 20112020055
+ * ------------------------------------------
+ */
+package nucleo;
+
+/**
+ * Clase que representa un lugar donde un bus realiza una parada dentro de una ruta
+ * @author Juan Pablo Moreno Rico - 20111020059
+ * @author Anderson Orlando Ramirez Lamprea - 20111020072
+ * @author Karen Vanessa Angulo Sogamoso - 20112020055
+ * @version 1.0
+ */
+public class Parada extends Estacion implements AccesoaDatos{
+
+    /**
+     * Crea una nueva parada para una ruta
+     */
+    public Parada () {
+    }
+
+	@Override
+	public String guardarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String buscarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
