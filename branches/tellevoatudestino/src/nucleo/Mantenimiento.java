ef98c1728bda80a375ed1d5c4416e023a5bff282
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-19 02:01:03 +0000

    Clase Mantenimiento, preliminar.

---
+++ branches/tellevoatudestino/src/nucleo/Mantenimiento.java
@@ -0,0 +1,33 @@
+/* 
+ * ------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas 
+ * 
+ * Mantenimiento.java
+ * 
+ * Proyecto de primer corte de Programacion Avanzada
+ * Presentado por:
+ * Juan Pablo Moreno Rico - 20111020059
+ * Anderson Orlando Ramirez Lamprea - 20111020072
+ * Karen Vanessa Angulo Sogamoso - 20112020055
+ * ------------------------------------------
+ */
+package nucleo;
+
+/**
+ * Clase que representa el estado del mantenimiento de un vehiculo de transporte
+ * @author Juan Pablo Moreno Rico - 20111020059
+ * @author Anderson Orlando Ramirez Lamprea - 20111020072
+ * @author Karen Vanessa Angulo Sogamoso - 20112020055
+ * @version 1.0
+ */
+public class Mantenimiento {
+
+    /**
+     * Crea un nuevo estado de mantenimiento
+     */
+    public Mantenimiento () {
+    }
+
+}
