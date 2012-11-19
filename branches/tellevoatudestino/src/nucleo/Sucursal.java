c401cd3d28809392692ab70da0b5c57dc2425f2b
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-19 02:05:17 +0000

    Clase sucursal, preliminar

---
+++ branches/tellevoatudestino/src/nucleo/Sucursal.java
@@ -0,0 +1,58 @@
+/* 
+ * ------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas 
+ * 
+ * Sucursal.java
+ * 
+ * Proyecto de primer corte de Programacion Avanzada
+ * Presentado por:
+ * Juan Pablo Moreno Rico - 20111020059
+ * Anderson Orlando Ramirez Lamprea - 20111020072
+ * Karen Vanessa Angulo Sogamoso - 20112020055
+ * ------------------------------------------
+ */
+package nucleo;
+
+/**
+ * Clase que representa una oficina de la empresa que tambien funciona como estacion
+ * @author Juan Pablo Moreno Rico - 20111020059
+ * @author Anderson Orlando Ramirez Lamprea - 20111020072
+ * @author Karen Vanessa Angulo Sogamoso - 20112020055
+ * @version 1.0
+ */
+public class Sucursal extends Estacion implements AccesoaDatos{
+
+    /**
+     * Crea una nueva sucursal de la empresa
+     */
+    public Sucursal () {
+    }
+
+    @Override
+    public String guardarRegistro() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+    
+    @Override
+    public String eliminarRegistro() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+    
+    @Override
+    public String actualizarRegistro() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+    
+    @Override
+    public String buscarRegistro() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+}
+
