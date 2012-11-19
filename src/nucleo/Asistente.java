57956b696bf7644d739b1bfd72d8645b6d3bcd7f
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-19 01:56:22 +0000

    Clase asistente, preliminar

---
+++ src/nucleo/Asistente.java
@@ -0,0 +1,58 @@
+/* 
+ * ------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas 
+ * 
+ * Asistente.java
+ * 
+ * Proyecto de primer corte de Programacion Avanzada
+ * Presentado por:
+ * Juan Pablo Moreno Rico - 20111020059
+ * Anderson Orlando Ramirez Lamprea - 20111020072
+ * Karen Vanessa Angulo Sogamoso - 20112020055
+ * ------------------------------------------
+ */
+package nucleo;
+
+/**
+ * Clase que representa al asistente del conductor del bus
+ * @author Juan Pablo Moreno Rico - 20111020059
+ * @author Anderson Orlando Ramirez Lamprea - 20111020072
+ * @author Karen Vanessa Angulo Sogamoso - 20112020055
+ * @version 1.0
+ */
+public class Asistente extends Persona implements AccesoaDatos{
+
+    /**
+     * Crea un nuevo asistente 
+     */
+    public Asistente () {
+    }
+
+	@Override
+	public String guardarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String eliminarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String actualizarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String buscarRegistro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
