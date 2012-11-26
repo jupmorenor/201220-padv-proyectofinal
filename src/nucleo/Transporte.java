308e50593d7b26f14deccb4ed58879e4a26de2ae
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-11-26 04:15:43 +0000

    Refinamiento de la implementación de los métodos de la interfaz AccesoaDatos.

--- src/nucleo/Transporte.java
+++ src/nucleo/Transporte.java
@@ -22,116 +22,139 @@ package nucleo;
  * @author Karen Vanessa Angulo Sogamoso - 20112020055
  * @version 1.0
  */
-public abstract class Transporte {
+public abstract class Transporte implements AccesoaDatos{
 
-    /**
-     * Cantidad de pasajeros que puede transportar 
-     */
-    private int capacidad;
+	/**
+	 * Cantidad de pasajeros que puede transportar 
+	 */
+	private int capacidad;
 
-    /**
-     * Numero de matricula del vehiculo
-     */
-    private String placa;
+	/**
+	 * Numero de matricula del vehiculo
+	 */
+	private String placa;
 
-    /**
-     * Codigo del vehiculo dentro de la empresa
-     */
-    private int codigoInterno;
+	/**
+	 * Codigo del vehiculo dentro de la empresa
+	 */
+	private String codigoInterno;
 
-    /**
-     * Cantidad de combustible que consume el vehiculo por kilometro
-     */
-    private float consumoCombustible;
+	/**
+	 * Cantidad de combustible que consume el vehiculo por kilometro
+	 */
+	private float consumoCombustible;
 
-    /**
-     * Registro del estado de mantenimiento
-     */
-    private Mantenimiento mMantenimiento;
+	/**
+	 * Registro del estado de mantenimiento
+	 */
+	private Mantenimiento mMantenimiento;
 
-    /**
-     * Crea un nuevo vehiculo de transporte
-     */
-    public Transporte () {
-    }
+	/**
+	 * Crea un nuevo vehiculo de transporte
+	 */
+	public Transporte () {
+	}
 
-    /**
-     * Devuelve la cantidad de pasajeros que puede transportar
-     * @return capacidad
-     */
-    public int getCapacidad () {
-        return capacidad;
-    }
+	/**
+	 * Devuelve la cantidad de pasajeros que puede transportar
+	 * @return capacidad
+	 */
+	public int getCapacidad () {
+		return capacidad;
+	}
 
-    /**
-     * Establece la cantidad de pasajeros que puede transportar
-     * @param cap cantidad de pasajeros
-     */
-    public void setCapacidad (int cap) {
-        this.capacidad = cap;
-    }
-    
-    /**
-     * Devuelve el numero de matricula del vehiculo
-     * @return placa
-     */
-    public String getPlaca () {
-        return placa;
-    }
+	/**
+	 * Establece la cantidad de pasajeros que puede transportar
+	 * @param cap cantidad de pasajeros
+	 */
+	public void setCapacidad (int cap) {
+		this.capacidad = cap;
+	}
 
-    /**
-     * Establece el numero de matricula del vehiculo
-     * @param pl numero de matricula
-     */
-    public void setPlaca (String pl) {
-        this.placa = pl;
-    }
+	/**
+	 * Devuelve el numero de matricula del vehiculo
+	 * @return placa
+	 */
+	public String getPlaca () {
+		return placa;
+	}
 
-    /**
-     * Devuelve el codigo del vehiculo dentro de la empresa
-     * @return codigoInterno
-     */
-    public int getCodigoInterno () {
-        return codigoInterno;
-    }
+	/**
+	 * Establece el numero de matricula del vehiculo
+	 * @param pl numero de matricula
+	 */
+	public void setPlaca (String pl) {
+		this.placa = pl;
+	}
 
-    /**
-     * Establece el codigo del vehiculo dentro de la empresa
-     * @param cod codigo del vehiculo
-     */
-    public void setCodigoInterno (int cod) {
-        this.codigoInterno = cod;
-    }
+	/**
+	 * Devuelve el codigo del vehiculo dentro de la empresa
+	 * @return codigoInterno
+	 */
+	public String getCodigoInterno () {
+		return codigoInterno;
+	}
 
-    /**
-     * Devuelve la cantidad de combustible que consume el vehiculo por kilometro
-     * @return consumoCombustible
-     */
-    public float getConsumoCombustible () {
-        return consumoCombustible;
-    }
+	/**
+	 * Establece el codigo del vehiculo dentro de la empresa
+	 * @param cod codigo del vehiculo
+	 */
+	public void setCodigoInterno (String cod) {
+		this.codigoInterno = cod;
+	}
 
-    /**
-     * Establece la cantidad de combustible que consume el vehiculo por kilometro
-     * @param con combustible que consume
-     */
-    public void setConsumoCombustible (float con) {
-        this.consumoCombustible = con;
-    }
+	/**
+	 * Devuelve la cantidad de combustible que consume el vehiculo por kilometro
+	 * @return consumoCombustible
+	 */
+	public float getConsumoCombustible () {
+		return consumoCombustible;
+	}
 
-    /**
-     * Devuelve el estado de mantenimiento del vehiculo
-     * @return mMantenimiento
-     */
-    public Mantenimiento getMantenimiento () {
-        return mMantenimiento;
-    }
+	/**
+	 * Establece la cantidad de combustible que consume el vehiculo por kilometro
+	 * @param con combustible que consume
+	 */
+	public void setConsumoCombustible (float con) {
+		this.consumoCombustible = con;
+	}
 
-    /**
-     * Establece el estado de mantenimiento del vehiculo
-     * @param man estado de mantenimiento
-     */
-    public void setMantenimiento (Mantenimiento man) {
-        this.mMantenimiento = man;
-    }
+	/**
+	 * Devuelve el estado de mantenimiento del vehiculo
+	 * @return mMantenimiento
+	 */
+	public Mantenimiento getMantenimiento () {
+		return mMantenimiento;
+	}
+
+	/**
+	 * Establece el estado de mantenimiento del vehiculo
+	 * @param man estado de mantenimiento
+	 */
+	public void setMantenimiento (Mantenimiento man) {
+		this.mMantenimiento = man;
+	}
+
+	@Override
+	public abstract String guardarRegistro();
+
+	@Override
+	public String eliminarRegistro() {
+		String cadena = "delete from transporte where(placa='" + this.getPlaca() + "');";
+		return cadena;
+	}
+
+	@Override
+	public String actualizarRegistro() {
+		String cadena = "update transporte set codigoInterno='" + this.getCodigoInterno() 
+				+ "' where(placa='" + this.getPlaca() + "');";	
+		return cadena;
+	}
+
+	@Override
+	public String buscarRegistro() {
+		String cadena = "select * from transporte where(placa='" + this.getPlaca() + "');";    	
+		return cadena;
+	}
+
 }
