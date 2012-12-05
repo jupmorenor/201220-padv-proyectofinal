aef913101c87cdabc21c335e282e89b7e009a755
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-05 16:03:44 +0000

    Correccion de espacios XD

--- tags/Atentificacion/src/db/script.sql
+++ tags/Atentificacion/src/db/script.sql
@@ -1,12 +1,8 @@
 create database autenticacionUsuarios_kja;
-
 use autenticacionUsuarios_kja;
-
-create table Usuarios
-(usuario varchar(12), password varchar(12), roll varchar(15));
+create table Usuarios (usuario varchar(12), password varchar(12), roll varchar(15));
 
 alter table Usuarios
 add primary key (usuario);
 
-insert into Usuarios
-values('admin', '12345', 'administrador');
+insert into Usuarios values('admin', '12345', 'administrador');
