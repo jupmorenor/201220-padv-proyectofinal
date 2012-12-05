55c592719d9496b23de24e959a580147f4af2efd
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-05 21:35:58 +0000

    Clase del usuario que va a crear una cuenta

---
+++ tags/Atentificacion/src/autenticador/UsuarioNuevo.java
@@ -0,0 +1,44 @@
+/* 
+ * ------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas 
+ * 
+ * UsuarioNuevo.java
+ * 
+ * Proyecto de primer corte de Programacion Avanzada
+ * Presentado por:
+ * Juan Pablo Moreno Rico - 20111020059
+ * Anderson Orlando Ramirez Lamprea - 20111020072
+ * Karen Vanessa Angulo Sogamoso - 20112020055
+ * ------------------------------------------
+ */
+package autenticador;
+
+/**
+ * Clase que registra un nuevo usuario en la base de datos
+ * @author Juan Pablo Moreno Rico - 20111020059
+ * @author Anderson Orlando Ramirez Lamprea - 20111020072
+ * @author Karen Vanessa Angulo Sogamoso - 20112020055
+ * @version 1.0
+ */
+public class UsuarioNuevo extends Usuario {
+
+	@Override
+	public String verificarUsuario() {
+		String cadena = "select * from usuarios where(usuario='" + this.getNombre()
+				+ "');";
+		return cadena;
+	}
+	
+	/**
+	 * Comando de MySQL que ingresa un nuevo registro a la base de datos de usuarios
+	 * @return cadena
+	 */
+	public String registrarUsuario(String tipo) {
+		String cadena = "insert into usuarios values('"+ this.getNombre() + "','"
+				+ this.getPassword() + "','" + tipo + "');";
+		return cadena;
+	}
+
+}
