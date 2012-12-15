630d08dac214e294b2778684b22f117eaae03ea8
jupmorenor@gmail.com <jupmorenor@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-15 19:43:14 +0000

    Se agrega la implementación del nuevo método que modifica la BD

--- src/autenticador/UsuarioExistente.java
+++ src/autenticador/UsuarioExistente.java
@@ -25,9 +25,16 @@ package autenticador;
 public class UsuarioExistente  extends Usuario{
 	
 	@Override
-	public String verificarUsuario() {
+	public String consultarUsuario() {
 		String cadena = "select * from usuarios where(usuario='" 
 				+ this.getNombre() + "' and password='" + this.getPassword() + "');";
+		return cadena;
+	}
+
+	@Override
+	public String modificarRegistro() {
+		String cadena = "update usuarios set password='" + this.getPassword() 
+				+ "' where(nombre='" + this.getNombre() + "');";
 		return cadena;
 	}
 	
