fbc333c3fce567388ecda49fd38f014a2876d500
karenv0207@gmail.com <karenv0207@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-18 22:46:39 +0000

    se adiciona la tabla personaBingo y tipo persona se generan las respectivas ralaciones entre las tablas

--- trunk/Bingo/src/db/Script BD Bingo.sql
+++ trunk/Bingo/src/db/Script BD Bingo.sql
@@ -1,14 +1,21 @@
 CREATE DATABASE bingo;
 USE bingo;
-CREATE TABLE Bingo( idBingo INT NOT NULL AUTO_INCREMENT, descBingo VARCHAR(30), PRIMARY KEY (idBingo) ); 
-CREATE TABLE Carton( idCarton INT NOT NULL AUTO_INCREMENT, descCarton VARCHAR(30), fk_idBingo INT NOT NULL, PRIMARY KEY (idCarton) );
-CREATE TABLE Balotas( idBalotas INT NOT NULL AUTO_INCREMENT, letra VARCHAR(1) NOT NULL, numero INT(2) NOT NULL, fk_idCarton INT NOT NULL, PRIMARY KEY (idBalotas) );
-CREATE TABLE BalotasCorrectas( idBalotaCorrec INT NOT NULL AUTO_INCREMENT, letra VARCHAR(1) NOT NULL, numero INT(2) NOT NULL, fk_idBingo INT NOT NULL, PRIMARY KEY (idBalotaCorrec) );
-
+CREATE TABLE Bingo( idBingo INT NOT NULL AUTO_INCREMENT, descBingo VARCHAR(30), PRIMARY KEY (idBingo)); 
+CREATE TABLE Carton( idCarton INT NOT NULL AUTO_INCREMENT, descCarton VARCHAR(30),puntuacion INT, fk_idBingo INT NOT NULL, fk_idUsuario INT NOT NULL, PRIMARY KEY (idCarton));
+CREATE TABLE Balotas( idBalotas INT NOT NULL AUTO_INCREMENT, letra VARCHAR(1) NOT NULL, numero INT NOT NULL, fk_idCarton INT NOT NULL, PRIMARY KEY (idBalotas));
+CREATE TABLE BalotasCorrectas( idBalotaCorrec INT NOT NULL AUTO_INCREMENT, letra VARCHAR(1) NOT NULL, numero INT NOT NULL, fk_idBingo INT NOT NULL, PRIMARY KEY (idBalotaCorrec));
+CREATE TABLE personabingo (idUsuario INT NOT NULL AUTO_INCREMENT, nombreUsuario VARCHAR(30), fk_idTipoPersona INT NOT NULL, PRIMARY KEY (idUsuario));
+CREATE TABLE tipoPersona (idTipoPersona INT NOT NULL AUTO_INCREMENT, descripTipoPersona VARCHAR(20), PRIMARY KEY (idTipoPersona));
+ 
 ALTER TABLE Carton
 ADD CONSTRAINT fk_Bingo_Carton_1 
 FOREIGN KEY (fk_idBingo) 
 REFERENCES Bingo(idBingo); 
+
+ALTER TABLE Carton 
+ADD CONSTRAINT fk_Usuario_Bingo_1 
+FOREIGN KEY (fk_idUsuario) 
+REFERENCES usuariobingo(idUsuario);
 
 ALTER TABLE BalotasCorrectas 
 ADD CONSTRAINT fk_Bingo_BalotaCorrect_1 
@@ -19,3 +26,8 @@ ALTER TABLE Balotas
 ADD CONSTRAINT fk_carton_balotas_1 
 FOREIGN KEY (fk_idCarton) 
 REFERENCES Carton(idCarton);
+
+ALTER TABLE personabingo
+  ADD CONSTRAINT fk_TipoPersona_PersonaBingo1
+  FOREIGN KEY (idTipoPersona_fk)
+  REFERENCES tipoPersona(idTipoPersona);
