7dcdd33f3bc46cbd484169cc65c53e20d4543e0f
karenv0207@gmail.com <karenv0207@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-18 23:42:31 +0000

    se modifica la consulta incluyendo nuevas tablas

--- trunk/Bingo/src/db/consultaGanadorBingo.sql
+++ trunk/Bingo/src/db/consultaGanadorBingo.sql
@@ -1,6 +1,8 @@
-SELECT b.descBingo, c.descCarton,a.letra, a.numero, d.letra, d.numero
-FROM Bingo b, Carton c, balotasCorrectas a, balotas d
+SELECT DISTINCT u.nombreUsuario, b.descBingo, c.descCarton,a.letra, a.numero, d.letra, d.numero
+FROM Bingo b, Carton c, balotasCorrectas a, balotas d, personabingo u, partidasganadas p, tipopersona t
 WHERE b.idBingo = a.fk_idBingo AND
 b.idBingo=c.fk_idBingo AND
 c.idCarton=d.fk_idCarton AND
+u.idUsuario=c.fk_idUsuario AND
+t.idTipoPersona=u.fk_idTipoPersona AND
 a.letra=d.letra AND a.numero=d.numero;
