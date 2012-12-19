006e0cbe465a551070e9adfe13375116229f3ab4
karenv0207@gmail.com <karenv0207@gmail.com@f624e6e2-2a8f-5492-280d-3d2164cf370f>
2012-12-19 00:00:38 +0000

    se modifica debido a q estaban apareciendo registros repetidos, se corrige el error en la consulta

--- trunk/Bingo/src/db/consultaGanadorBingo.sql
+++ trunk/Bingo/src/db/consultaGanadorBingo.sql
@@ -1,8 +1,8 @@
-SELECT DISTINCT u.nombreUsuario, b.descBingo, c.descCarton,a.letra, a.numero, d.letra, d.numero
-FROM Bingo b, Carton c, balotasCorrectas a, balotas d, personabingo u, partidasganadas p, tipopersona t
+SELECT u.nombreUsuario, b.descBingo, c.descCarton,a.letra, a.numero, d.letra, d.numero
+FROM Bingo b, Carton c, balotasCorrectas a, balotas d, personabingo u, tipopersona t
 WHERE b.idBingo = a.fk_idBingo AND
 b.idBingo=c.fk_idBingo AND
 c.idCarton=d.fk_idCarton AND
 u.idUsuario=c.fk_idUsuario AND
 t.idTipoPersona=u.fk_idTipoPersona AND
-a.letra=d.letra AND a.numero=d.numero;
+a.letra=d.letra AND a.numero=d.numero
